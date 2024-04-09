dad8a566ba5dcce40640e679ce55672efadc4b4b
Steve Kinney <hello@stevekinney.net>
2024-04-09 12:24:49 -0600

    Add variants to the button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,6 +1,8 @@
 import { ComponentProps } from 'react';
 
-type ButtonProps = ComponentProps<'button'>;
+type ButtonProps = ComponentProps<'button'> & {
+  variant?: 'primary' | 'secondary' | 'destructive';
+};
 
 export const Button = (props: ButtonProps) => {
   return <button {...props} />;
