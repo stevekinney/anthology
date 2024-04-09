043bf4ad2ce58241b05e36c5b8741d5f14fa4051
Steve Kinney <hello@stevekinney.net>
2024-04-09 13:49:33 -0600

    Add @addons/themes

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from '@storybook/react';
+import { withThemeByDataAttribute } from '@storybook/addon-themes';
 
 import '../src/index.css';
 
@@ -11,6 +12,16 @@ const preview: Preview = {
       },
     },
   },
+  decorators: [
+    withThemeByDataAttribute({
+      defaultTheme: 'light',
+      themes: {
+        light: 'light',
+        dark: 'dark',
+      },
+      attributeName: 'data-mode',
+    }),
+  ],
 };
 
 export default preview;
