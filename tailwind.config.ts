5ade636f150ed45fb3d94a431333060c0addf9f0
Steve Kinney <hello@stevekinney.net>
2024-04-09 13:23:43 -0600

    Add configuration for dark mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from 'tailwindcss';
 
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx,mdx,html}'],
+  darkMode: ['class', '[data-mode="dark"]'],
   theme: {
     extend: {},
   },
