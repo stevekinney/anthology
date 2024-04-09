ae0dc5f0e82a154d9dfd1c99f3a27d3b1893e2d5
Steve Kinney <hello@stevekinney.net>
2024-04-09 12:40:34 -0600

    Add basic styles to button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,9 +1,11 @@
 import { ComponentProps } from 'react';
 
+import styles from './button.module.css';
+
 type ButtonProps = ComponentProps<'button'> & {
   variant?: 'primary' | 'secondary' | 'destructive';
 };
 
 export const Button = (props: ButtonProps) => {
-  return <button {...props} />;
+  return <button className={styles.button} {...props} />;
 };
