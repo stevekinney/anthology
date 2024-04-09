2f31cb548cd2e9a6667d4f3d455229cfef9ccd5a
Steve Kinney <hello@stevekinney.net>
2024-04-09 14:31:09 -0600

    Add semantic colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,20 @@
 import type { Config } from 'tailwindcss';
+import colors from 'tailwindcss/colors';
 
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx,mdx,html}'],
   darkMode: ['class', '[data-mode="dark"]'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.indigo,
+        success: colors.green,
+        warning: colors.amber,
+        danger: colors.red,
+        info: colors.blue,
+        surface: colors.slate,
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
