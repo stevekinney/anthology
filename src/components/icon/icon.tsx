fc968e98f0d8f52cdb0602b31ad76a836b3536d9
Steve Kinney <hello@stevekinney.net>
2024-04-14 13:39:08 -0600

    Update icon.tsx

--- src/components/icon/icon.tsx
+++ src/components/icon/icon.tsx
@@ -1,46 +1,76 @@
 import {
   type LucideIcon,
   AlertTriangle,
+  Bug,
   Check,
+  CheckCircle2,
+  ChevronDown,
+  Clipboard,
+  ExternalLink,
+  Flame,
+  Hash,
+  Heart,
   HelpCircle,
   Info,
-  X,
-  Zap,
-  ChevronDown,
+  Link,
+  List,
+  Pencil,
+  Plus,
+  Quote,
   Skull,
   Star,
-  ExternalLink,
-  Heart,
+  X,
+  Zap,
 } from 'lucide-react';
 import { ComponentProps } from 'react';
 
 export type IconProps = ComponentProps<LucideIcon> & {
   type:
-    | 'warning'
+    | 'bug'
     | 'check'
+    | 'chevron'
+    | 'clipboard'
+    | 'external'
+    | 'flame'
+    | 'hash'
+    | 'heart'
     | 'help'
     | 'info'
-    | 'x'
-    | 'zap'
-    | 'chevron'
+    | 'link'
+    | 'list'
+    | 'pencil'
+    | 'plus'
+    | 'quote'
     | 'skull'
     | 'star'
-    | 'external'
-    | 'heart';
+    | 'success'
+    | 'warning'
+    | 'x'
+    | 'zap';
 };
 
 const iconComponents: Record<IconProps['type'], LucideIcon> = {
-  warning: AlertTriangle,
+  bug: Bug,
   check: Check,
+  chevron: ChevronDown,
+  clipboard: Clipboard,
+  external: ExternalLink,
+  flame: Flame,
+  hash: Hash,
+  heart: Heart,
   help: HelpCircle,
   info: Info,
-  x: X,
-  zap: Zap,
-  chevron: ChevronDown,
+  link: Link,
+  list: List,
+  pencil: Pencil,
+  plus: Plus,
+  quote: Quote,
   skull: Skull,
   star: Star,
-  external: ExternalLink,
-  heart: Heart,
+  success: CheckCircle2,
+  warning: AlertTriangle,
+  x: X,
+  zap: Zap,
 };
 
 export const Icon = ({ type, ...props }: IconProps) => {
