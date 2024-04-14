1e6be20d687539d0769efd89d3c97a52589b2adf
Steve Kinney <hello@stevekinney.net>
2024-04-14 14:31:56 -0600

    Integrate fonts and color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,20 @@
 import type { Config } from 'tailwindcss';
-import colors from 'tailwindcss/colors';
+import { colors, white, black, transparent, currentColor } from './src/tokens/colors';
 
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx,mdx,html}'],
   darkMode: ['class', '[data-mode="dark"]'],
   theme: {
+    colors: {
+      ...colors,
+      white,
+      black,
+      transparent,
+      currentColor,
+    },
     extend: {
-      colors: {
-        primary: colors.indigo,
-        success: colors.green,
-        warning: colors.amber,
-        danger: colors.red,
-        info: colors.blue,
-        surface: colors.slate,
+      fontFamily: {
+        sans: ['Inter Variable', 'sans-serif'],
       },
     },
   },
