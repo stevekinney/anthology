524576ba1161bd44286efde8a5501d125dea1f34
Steve Kinney <hello@stevekinney.net>
2024-04-14 08:19:52 -0600

    Organize storybook components

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { Button } from './button';
 
 const meta = {
-  title: 'Button',
+  title: 'Components/Button',
   component: Button,
   args: {
     children: 'Button',
