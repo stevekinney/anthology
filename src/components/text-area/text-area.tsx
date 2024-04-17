2b9f531e1b9cf35f1363a2d2fd1ce4d3d5a8fdb3
Steve Kinney <hello@stevekinney.net>
2024-04-17 04:17:12 -0500

    Update text-area.tsx

--- src/components/text-area/text-area.tsx
+++ src/components/text-area/text-area.tsx
@@ -1,11 +1,11 @@
 import clsx from 'clsx';
-import type { ComponentProps } from 'react';
+import { useMemo, useState, type ComponentProps } from 'react';
 
 type TextAreaProps = ComponentProps<'textarea'> & { label: string };
 
 export const TextArea = ({ label, required, maxLength, ...props }: TextAreaProps) => {
-  const tooLong = maxLength && String(props.value).length > maxLength;
-  const length = props.value ? String(props.value).length : 0;
+  const [value, setValue] = useState(String(props.value || ''));
+  const tooLong = useMemo(() => maxLength && value.length > maxLength, [value, maxLength]);
 
   return (
     <label className="flex flex-col gap-1.5">
@@ -19,14 +19,19 @@ export const TextArea = ({ label, required, maxLength, ...props }: TextAreaProps
       </span>
 
       <textarea
-        className="block w-full gap-2 rounded-md bg-transparent bg-white p-4 text-sm placeholder-slate-400 shadow-sm ring-1 ring-inset ring-slate-500 focus:bg-primary-50 focus:outline-none focus:ring-2 focus:ring-primary-600 disabled:cursor-not-allowed disabled:bg-slate-50 dark:bg-slate-800 dark:placeholder-slate-300"
+        className={clsx(
+          'w-full gap-2 rounded-md bg-transparent bg-white p-4 text-sm placeholder-slate-400 shadow-sm ring-1 ring-inset ring-slate-500 invalid:bg-danger-50 focus:bg-primary-50 focus:outline-none focus:ring-2 focus:ring-primary-600 disabled:cursor-not-allowed disabled:bg-slate-50 dark:bg-slate-800 dark:placeholder-slate-300',
+          tooLong && 'ring-2 ring-danger-500',
+        )}
         {...props}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
       />
 
       {maxLength && (
-        <div className="flex justify-end text-xs text-slate-500">
-          <p>
-            {length || 0}/{maxLength}
+        <div className="gap-1.4 flex justify-end text-xs">
+          <p className={clsx(tooLong ? 'text-danger-500' : 'text-slate-500')}>
+            <span data-length={value.length}>{value.length}</span>/{maxLength}
           </p>
         </div>
       )}
